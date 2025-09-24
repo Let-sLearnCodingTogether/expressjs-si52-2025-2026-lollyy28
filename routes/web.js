686c742c6404a26a6c392269145bfb77951e0afb
lolly28 <jennychristine2806@gmail.com>
2025-09-24 20:41:49 +0700

    Create web.js

---
+++ routes/web.js
@@ -0,0 +1,18 @@
+import express from "express"
+
+const web = express.Router()
+
+web.get('/', (req, res) => {
+    res.render('index')
+})
+web.get('/:username', (req, res) => {
+const username = req.params.username
+
+    res.render('public-profile', {
+        title : username,
+        username : username,
+        bio : "Ahyeon"
+    })
+})
+
+export default web
