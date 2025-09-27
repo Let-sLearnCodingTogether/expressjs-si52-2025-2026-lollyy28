a0c60fa3165090092837e19671dc86e961f67c0d
lolly28 <>
2025-09-27 18:47:02 +0700

    Update web.js

--- routes/web.js
+++ routes/web.js
@@ -5,14 +5,6 @@ const web = express.Router()
 web.get('/', (req, res) => {
     res.render('index')
 })
-web.get('/:username', (req, res) => {
-const username = req.params.username
-
-    res.render('public-profile', {
-        title : username,
-        username : username,
-        bio : "Ahyeon"
-    })
-})
+web.get('/:username', profileController.publicProfile)
 
 export default web
