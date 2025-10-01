2a45008c2e5bbd7bd807990af8938fb4f2973cd9
lolly28 <jennychristine2806@gmail.com>
2025-10-01 20:42:32 +0700

    Create ProfileModel.js

---
+++ models/ProfileModel.js
@@ -0,0 +1,28 @@
+import mongoose, { trusted } from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        displayName : {
+            type : String,
+            required : [true, "Profile picture wajib di isi"]
+        },
+        profilePicture : {
+            type : String,
+            required : [true, "Profile picture wajib di isi"]
+        },
+        bio : {
+            type : String,
+            minLeght : [10, "minimal 10 karakter"],
+            maxleght : [150, "maximal 150 karakter"],
+            required : [true, "Profile picture wajib di isi"],
+            trim: true
+        },
+        user : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    }, {
+        timestamps : true
+    }
+)
