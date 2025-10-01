930365b49adb204def79c5b0e00a87ff3fad3231
lolly28 <jennychristine2806@gmail.com>
2025-10-01 20:42:29 +0700

    Create linkModel.js

---
+++ models/linkModel.js
@@ -0,0 +1,32 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const LinkSchema = new mongoose.Schema(
+    {title : {
+        type : String,
+        required : [true, "Title di isi"],
+        trim : true
+    },
+    icon :   {
+        type : String,
+        required : [true, "Icon di isi"],
+        trim : true
+    },
+    uri :  {
+        type : String,
+        required : [true, "URL di isi"],
+        trim : true
+    },
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    }
+    },
+    {
+        timestamps : true
+    }
+)
+
+const LinkModel = Mongoose.model('Link', LinkSchema)
+
+export default LinkModel
