b7620034d66191d0aa1be996bd1e1011e6385f34
lollyy28 <>
2025-10-04 18:47:42 +0700

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,20 @@
+import UserModel from "../models/userModel.js";
+
 export const register = async (req, res) => {
     try {
+        //Untuk mengambil body atau data dari request
+        const registerData = req.body
+
+        console.log(registerData);
+
+        await UserModel.create({
+            username : registerData.username,
+            email : registerData.email,
+            password : registerData.password
+
+        })
+
+
         res.status(500).json({
             message : "Berhasil register, silahkan login",
             data : null
