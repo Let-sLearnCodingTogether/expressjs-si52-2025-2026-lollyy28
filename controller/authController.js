cab41de107c966f5c0d3210eaecaedc96e4188ca
Lolly28 <jennychristine2806@gmail.com>
2025-10-08 20:41:39 +0700

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -7,15 +7,18 @@ export const register = async (req, res) => {
 
         console.log(registerData);
 
+        const hashPassword =hash(registerData.password)
+
+
         await UserModel.create({
             username : registerData.username,
             email : registerData.email,
-            password : registerData.password
+            password : hashPassword
 
         })
 
 
-        res.status(500).json({
+        res.status(201).json({
             message : "Berhasil register, silahkan login",
             data : null
         })
@@ -24,5 +27,35 @@ export const register = async (req, res) => {
             message : e.message,
             data : null
         })
+    }
+}
+
+export const login = async(req, res) => {
+    try{
+        const loginData = req.body
+
+        //mencari user berdasarkan email
+        const user = await UserModel.findOne({
+            email : loginData.email
+
+        })
+
+
+        //Jika user tidak ditemukan
+        if (!user){
+            res.status(404).json({
+                message : "User not found",
+                data : null
+            })
+        }
+
+        //membandingkan password yang ada di dalam db dengan request
+        if(user.password == loginData.password){
+        }
+    }catch (error){
+    res.status(500).json({
+        message: e.message,
+        data : null
+    })
     }
 }
