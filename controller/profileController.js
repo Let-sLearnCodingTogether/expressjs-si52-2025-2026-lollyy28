d91e8919b86407f87dc7613253c2aed347b5ec8a
Lolly28 <jennychristine2806@gmail.com>
2025-10-22 19:59:28 +0700

    Update profileController.js

--- controller/profileController.js
+++ controller/profileController.js
@@ -9,6 +9,10 @@ const username = req.params.username
 
 export const privateProfile = async (req, res) => {
     try {
+        res.status(200).json({
+            message:"Private profile berhasil di akses",
+            data: null,
+        });
 
     } catch (error) {
         res.status(500).json({
