fbf3291ee4c4c04952906a9673ef7929538b7534
Lolly28 <jennychristine2806@gmail.com>
2025-10-22 19:59:31 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 import express from "express"
 import * as authController from "../controller/authController.js"
+import * as profileController from "../controller/profileController.js"
 
 const api = express.Router()
 
 api.post('/register', authController.register)
 api.post('/login', authController.login)
 
+api.get('/me', profile)
 export default api
